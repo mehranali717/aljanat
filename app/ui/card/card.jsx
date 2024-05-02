3c9858ff7c9ed4911cafa8ec090bf0f3f20a3191
Mehran Ali Tehran <Mehranalee110@gmail.com>
2024-05-02 09:38:36 -0700

    card improve

--- app/ui/card/card.jsx
+++ app/ui/card/card.jsx
@@ -4,7 +4,14 @@ import Image from "next/image";
 const Card = ({ item }) => {
   return (
     <div className={styles.container}>
-      <Image src={item.img} width={260} height={160} alt="property image" className={styles.img}/>
+      <Image
+        src={item.img}
+        width={260}
+        height={160}
+        alt="property image"
+        className={styles.img}
+      />
+      <span className={styles.action}>{item.action}</span>
       <span className={styles.cost}>{item.cost}</span>
       <h3 className={styles.title}>{item.title}</h3>
       <Link href="" className={styles.location}>
